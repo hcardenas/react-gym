fa8c1edebf45ab92b7c243a08c078079f9cee297
Yvanna Harris <yvannaharris@yvannas-air.broward.edu>
2017-11-21 18:55:29 -0500

    Added user name form firebase

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -9,7 +9,7 @@ export default class Nav extends Component {
 			<Navbar brand='logo' right>
 				<NavItem href='/'><Icon left>home</Icon>Home</NavItem>
 				<NavItem href='/public'><Icon left>public</Icon>Public</NavItem>
-				<Dropdown trigger={<NavItem><Icon left>person</Icon>Username</NavItem>}>
+				<Dropdown trigger={<NavItem><Icon left>person</Icon>{firebase.auth().currentUser.displayName}</NavItem>}>
 					<NavItem>Username</NavItem>
 					{(this.props.isUserlogged === false ? "" : <NavItem href='/' onClick={ () => {this.props.signOutUser()} }>Logout</NavItem>)}
 				</Dropdown>
