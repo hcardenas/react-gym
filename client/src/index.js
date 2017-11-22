c4d8308412a3b5ebf9294c3f273a6aa2c4cf2c85
Yvanna Harris <yvannaharris@yvannas-air.broward.edu>
2017-11-21 19:05:53 -0500

    Added firebase config to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import * as firebase from 'firebase';
+
+
+var config = {
+    apiKey: "AIzaSyDhvVfvQFFykgd_vms-ortZcKTBm1PjAuU",
+    authDomain: "react-gym-ae92c.firebaseapp.com",
+    databaseURL: "https://react-gym-ae92c.firebaseio.com",
+    projectId: "react-gym-ae92c",
+    storageBucket: "",
+    messagingSenderId: "946508433089"
+  };
+  
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
