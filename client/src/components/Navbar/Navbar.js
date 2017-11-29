9cdf8f4703bd7dc7254755b004d9225890eba8ed
Yvanna Harris <yvannaharris@yvannas-air.broward.edu>
2017-11-28 19:39:06 -0500

    Throw away

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,12 +5,14 @@ import * as firebase from 'firebase';
 
 export default class Nav extends Component {
 	render() {
+
+
 		return (
 			<Navbar brand='logo' right>
 				<NavItem href='/'><Icon left>home</Icon>Home</NavItem>
 				<NavItem href='/public'><Icon left>public</Icon>Public</NavItem>
 				<Dropdown trigger={<NavItem><Icon left>person</Icon>{firebase.auth().currentUser.displayName}</NavItem>}>
-					<NavItem>Username</NavItem>
+					<NavItem></NavItem>
 					{(this.props.isUserlogged === false ? "" : <NavItem href='/' onClick={ () => {this.props.signOutUser()} }>Logout</NavItem>)}
 				</Dropdown>
 			</Navbar>
