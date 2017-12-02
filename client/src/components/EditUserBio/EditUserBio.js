b214cc8f516fca5b537c8a7896d2d1e4960a4b60
Zachary Saltzman <zsaltzman@zacharys-mbp-2.broward.edu>
2017-12-02 12:48:15 -0500

    upload button working

--- client/src/components/EditUserBio/EditUserBio.js
+++ client/src/components/EditUserBio/EditUserBio.js
@@ -73,6 +73,15 @@ export default class UserBio extends Component {
 		})
 	};
 
+	handleImageSubmit = event=> {
+		event.preventDefault();
+		API.updateUser({
+			profile_pic: this.state.uploadedFileCloudinaryUrl
+		}, this.props.id).then(dbUser => {
+			console.log("Pic Updated");
+		})
+	};
+
 	render() {
 
 		console.log("ON RENDER BIO = " + this.props.bio);
@@ -97,6 +106,10 @@ export default class UserBio extends Component {
             					<img src={this.state.uploadedFileCloudinaryUrl} />
           					</div>}
         				</div>
+        				<br />
+        				<a onClick={this.handleImageSubmit} className="waves-effect waves-light btn">
+			            				Save
+			            			</a>
 
 		            </div>
 		            <form className="col m7">
