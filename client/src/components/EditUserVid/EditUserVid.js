fd6b93c7cdf2e9c5757a9a68590aabf4d7b05715
helmut cardenas <aleksey89col@gmail.com>
2017-12-05 17:02:00 -0500

    fixed tab padding on edivVidUser

--- client/src/components/EditUserVid/EditUserVid.js
+++ client/src/components/EditUserVid/EditUserVid.js
@@ -66,7 +66,7 @@ export default class EditUserVid extends Component {
 
 		return (
 			<div>
-				<Tabs className='tab-demo z-depth-1 center-align'>
+				<Tabs className='tab-demo z-depth-1 center-align tabPadding'>
 						<Tab title="Create Sessions" >
 							<br />
 							<CreateUserSessions
